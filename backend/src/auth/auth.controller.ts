c976c909eb6a5ccd24ee91b0532d210bf5821812
Iago3457 <iagobras19122007@gmail.com>
2026-06-13 15:08:59 -0300

    feat: controller for auth

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UsePipes, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ZodValidationPipe } from './zod-validation.pipe';
+import type { CadastroDto, LoginDto } from './dto/auth.schema';
+import { cadastroSchema, loginSchema } from './dto/auth.schema';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('cadastro')
+    @UsePipes(new ZodValidationPipe(cadastroSchema))
+    async cadastrar(@Body() dados: CadastroDto) {
+        return {mensagem: 'Dados validados com sucesso no cadastro!', dados};
+    }
+
+    @Post('login')
+    @UsePipes(new ZodValidationPipe(loginSchema))
+    async login(@Body() dados: LoginDto) {
+        return {mensagem: 'Dados validados com sucesso no login!', dados};
+    }
+}
