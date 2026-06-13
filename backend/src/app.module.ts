d8ce4339b40dc2b2b3486011b15c89cdffdef5f8
Iago3457 <iagobras19122007@gmail.com>
2026-06-13 14:55:26 -0300

    feat: zod schema for login and registration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AlunoModule } from './aluno/aluno.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AlunoModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
