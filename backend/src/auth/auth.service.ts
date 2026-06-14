bec12b71d9fad12701d4607537ab71b4e511ed1a
Iago3457 <iagobras19122007@gmail.com>
2026-06-14 20:43:48 -0300

    added missing information to prism table 'aluno'

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -11,6 +11,20 @@ export class AuthService {
     constructor(private readonly prisma: PrismaService, private readonly jwtService: JwtService) {}
 
     async cadastrar(dados: CadastroDto) {
+        const anoAtual = new Date().getFullYear().toString();
+        const ultimoAluno = await this.prisma.aluno.findFirst({
+            where : {
+                ra: { startsWith: anoAtual}
+            },
+            orderBy: { ra: 'desc' }
+
+        });
+        let proximoNumero = 1;
+        if (ultimoAluno) {
+            const ultimoNumeroSequencial = parseInt(ultimoAluno.ra.substring(4));
+            proximoNumero = ultimoNumeroSequencial + 1;
+        }
+        const raGerado = `${anoAtual}${String(proximoNumero).padStart(6, '0')}`;
         const alunoExistente = await this.prisma.aluno.findUnique({
             where: { email: dados.email },
         });
@@ -27,6 +41,9 @@ export class AuthService {
                 nome: dados.nome,
                 email: dados.email,
                 senha: senhaHash,
+                ra: raGerado,
+                periodo: '1', //atualizar depois para calcular o período com base no semestre de ingresso
+                semestre: `${anoAtual}.1`,  //atualizar depois para calcular o semestre de ingresso
             }
         });
         
